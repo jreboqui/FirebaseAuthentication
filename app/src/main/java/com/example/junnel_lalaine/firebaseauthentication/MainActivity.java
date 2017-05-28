ba0add068d10d391cdb5d78ff0ba2016ccd9964a
Jun Rebo <raijin.dhie@gmail.com>
2017-05-28 12:38:01 -0700

    fuck you android studio

---
+++ app/src/main/java/com/example/junnel_lalaine/firebaseauthentication/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.junnel_lalaine.firebaseauthentication;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+
+    //UI References
+    private EditText mEmail, mPassword;
+    private Button btnSignIn, btnSignOut, btnUploadImage;
+
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //declare buttons and edit texts in oncreate
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        btnSignIn = (Button) findViewById(R.id.email_sign_in_button);
+        btnSignOut = (Button) findViewById(R.id.email_sign_out_button);
+        btnUploadImage = (Button) findViewById(R.id.upload_image);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessages("Successfully signed in with: " + user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessages("Successfully signed out");
+                }
+                // ...
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if (!email.equals("") && !pass.equals("")) {
+                    mAuth.signInWithEmailAndPassword(email, pass);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No fields", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                toastMessages("Signing Out...");
+            }
+        });
+
+        btnUploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: Switching Activities.");
+                Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+        @Override
+        public void onStart () {
+            super.onStart();
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+
+        @Override
+        public void onStop () {
+            super.onStop();
+            if (mAuthListener != null) {
+                mAuth.removeAuthStateListener(mAuthListener);
+            }
+        }
+
+        private void toastMessages(String message){
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+
+}
